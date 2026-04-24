55ef6b5e749bbcbe5a790cfdf837c4fe9b28989b
Arpan-Ghosh07 <arpanghosh1029@gmail.com>
2026-04-24 23:35:08 +0530

    Adding Navbar

---
+++ app/components/ResumeCard.tsx
@@ -0,0 +1,6 @@
+const ResumeCard:()=>Element =()=>{
+    return(
+        <div>ResumeCard</div>
+    )
+}
+export default ResumeCard
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
-import type {Route} from "./+types/home";
-
+import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import ResumeCard from "../components/ResumeCard";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Resumind" },
@@ -9,12 +10,18 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-
+<Navbar />
 <section className="main-section">
   <div className="page-heading">
     <h1>Track your Applications & Resume Ratings</h1>
     <h2>Review your submissions and check AI-powered feedback.</h2>
   </div>
 </section>
+<div className="resumes-section">
+
+</div>
+    {resumes.map((resume) => (
+        <ResumeCard key={resume.id} resume={resume}/>
+    ))}
   </main>
 }
