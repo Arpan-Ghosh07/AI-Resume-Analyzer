246bee28c904f56de40b9bc2c08efbae78e2d844
Arpan-Ghosh07 <arpanghosh1029@gmail.com>
2026-04-25 23:42:10 +0530

    Authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,9 @@
 import Navbar from "~/components/Navbar";
 import {resumes} from "../../constants";
 import ResumeCard from "../components/ResumeCard";
+import {usePuterStore} from "~/lib/puter";
+import {useLocation, useNavigate} from "react-router";
+import {useEffect} from "react";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Resumind" },
@@ -9,8 +12,16 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const {auth}=usePuterStore();
+  const navigate = useNavigate();
+
+
+  useEffect(()=>{
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
+  },[auth.isAuthenticated])
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
 <Navbar />
+
 <section className="main-section">
   <div className="page-heading py-10">
     <h1>Track your Applications & Resume Ratings</h1>
