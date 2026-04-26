e47c7086e40e62dbd4805de28ff988d611cd61a0
Arpan-Ghosh07 <arpanghosh1029@gmail.com>
2026-04-26 23:51:13 +0530

    adding upload form and file section

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -2,23 +2,22 @@ import {usePuterStore} from "~/lib/puter";
 import {useEffect} from "react";
 import {useLocation, useNavigate} from "react-router";
 
-
-export const meta= ()=> ([
-    {title:'Resumind | Auth'},
-    {name:'description',content:'Log into your account'},
+export const meta = () => ([
+    { title: 'Resumind | Auth' },
+    { name: 'description', content: 'Log into your account' },
 ])
 
-const Auth=()=>{
-    const {isLoading,auth}=usePuterStore();
+const Auth = () => {
+    const { isLoading, auth } = usePuterStore();
     const location = useLocation();
-    const next=location.search.split('next=')[1];
+    const next = location.search.split('next=')[1];
     const navigate = useNavigate();
 
-
-    useEffect(()=>{
+    useEffect(() => {
         if(auth.isAuthenticated) navigate(next);
-    },[auth.isAuthenticated])
-    return(
+    }, [auth.isAuthenticated, next])
+
+    return (
         <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
             <div className="gradient-border shadow-lg">
                 <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
@@ -27,20 +26,20 @@ const Auth=()=>{
                         <h2>Log In to Continue Your Job Journey</h2>
                     </div>
                     <div>
-                        {isLoading?(
+                        {isLoading ? (
                             <button className="auth-button animate-pulse">
                                 <p>Signing you in...</p>
                             </button>
-                        ):(
+                        ) : (
                             <>
-                                {auth.isAuthenticated?(
+                                {auth.isAuthenticated ? (
                                     <button className="auth-button" onClick={auth.signOut}>
-                                       <p> Log Out</p>
+                                        <p>Log Out</p>
                                     </button>
-                                ):(
+                                ) : (
                                     <button className="auth-button" onClick={auth.signIn}>
                                         <p>Log In</p>
-                                        </button>
+                                    </button>
                                 )}
                             </>
                         )}
@@ -50,4 +49,5 @@ const Auth=()=>{
         </main>
     )
 }
+
 export default Auth
